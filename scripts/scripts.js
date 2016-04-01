0f8706ea391bf81a6293db318c2518bd3a8fefaf
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-01 10:00:44 -0600

    Working on styling the posts and added a factory for the posts.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,36 @@
 var app = angular.module('myApp', ['angularMoment']);
 
-app.controller('PostController', function($scope) {
-  $scope.view = {};
-  $scope.posts = [];
-  $scope.addPost = function() {
-    $scope.posts.push({
-      author: $scope.author,
-      title: $scope.title,
-      description: $scope.description,
-      image_url: $scope.image_url
+app.controller('PostController', function($scope, post) {
+  $scope.post = post;
+});
+
+app.factory('post', function postFactory() {
+  var post = {};
+  post.posts = [{
+    author: 'Mike Dee',
+    title: 'A Beautiful Waterfall',
+    description: 'It is thought that the underwater waterfall the Denmark Strait cataract is the largest waterfall by all measures with a drop of 11,500 feet or 3,500 m and a flow rate exceeding 175 million cubic feet (5.0 million cubic meters) per second, making it 350 times as voluminous as the extinct Guaíra Falls on the border of Brazil and Paraguay, which was once thought to be the most voluminous waterfall on Earth.',
+    image_url: 'http://rs255.pbsrc.com/albums/hh160/loewus9_photos/Waterfalls/1020Waterfall.jpg~c200',
+  }];
+  post.showSubmit = false;
+  post.addPost = function() {
+    post.posts.push({
+      author: post.author,
+      title: post.title,
+      description: post.description,
+      image_url: post.image_url
     });
-    $scope.author = '';
-    $scope.title = '';
-    $scope.description = '';
-    $scope.image_url = '';
-    console.log($scope.posts);
+    post.author = '';
+    post.title = '';
+    post.description = '';
+    post.image_url = '';
+    console.log(post.posts);
+    post.showSubmit = false;
   };
+  post.get = function() {
+    return post.posts;
+  };
+  return post;
 });
 
 app.controller('mainController', function() {
